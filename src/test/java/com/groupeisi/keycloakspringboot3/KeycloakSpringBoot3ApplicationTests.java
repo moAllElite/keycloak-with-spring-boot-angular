63c070442dd01b7788e2878aa34ccf790a03feb6
NIANG MOUHAMED <mouhamedn347@gmail.com>
2024-04-06 18:48:42 +0000

    Initial commit

---
+++ src/test/java/com/groupeisi/keycloakspringboot3/KeycloakSpringBoot3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.keycloakspringboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringBoot3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
